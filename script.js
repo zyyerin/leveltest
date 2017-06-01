a4581039ba936e1cee8a4bd2663ed5b3f1180e19
Erin Zhang <yzhang3@cca.edu>
2017-06-01 16:19:25 -0700

    0.0.3 stack

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var MIC;
-var HIGH_LEVEL;
-var TOP_POINT;
+
+var RECORD;
+var RANGE;
+
 var BELL_FILL;
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -9,57 +12,81 @@ function setup() {
     MIC = new p5.AudioIn();
     MIC.start();
     
-    HIGH_LEVEL = 0;
-    TOP_POINT = 300;
+    RECORD = 0;
+    RANGE = 400;
     BELL_FILL = 0;
 }
 
 function draw() {
+    let nob = 20;
+    let gap = 5;
+    let unitH = (RANGE+gap)/nob;
+    let blockW = 50;
+    let blockH = unitH - gap;
+    
+    // sensitivity adjustment
+    let vol = MIC.getLevel();
+    let currentLevel = Math.floor(map(vol, 0, 0.8, 0, 10));
+    
     // background setting
     background(240);
     noStroke();
-    // draw a ground
-    fill(0, 170, 255);
-    rect(0, height - 100, width, 100);    
+    
+    
+    
+    push();
+    translate(0, 150);
+    // VISUALIZOR starts here
+    
     // level container
     fill(255);
-    rect(width/2 - 25, height - 100, 50, -TOP_POINT);
+    for (let i = 0; i<nob; i += 1) {
+        rect(width/2 - blockW/2, RANGE + gap*2 - i*unitH, blockW, blockH);
+    }
     
-    
-    // Get the overall volume (between 0 and 1.0)
-    let vol = MIC.getLevel();
-    console.log(vol);
-    
-    // VISUALIZOR starts here
     // Draw a rectangle with height based on volume
-    let h = map(vol, 0, 1, 0, height);
-    
-    if (HIGH_LEVEL < h) {
-        HIGH_LEVEL = h;
+    if (RECORD < currentLevel) {
+        RECORD = currentLevel;
     }
     fill(255, 255, 0);
-    rect(width/2 - 25, height - 100, 50, -HIGH_LEVEL);
+    
+    for (let i = 0; i<RECORD; i += 1) {
+        rect(width/2 - blockW/2, RANGE + gap*2 - i*unitH, blockW, blockH);
+    }
+    
     
     // visualize live volume
+
+
     fill(255, 170, 0);
-    rect(width/2 - 25, height - 100, 50, -h);
+    for (let i = 0; i<currentLevel; i += 1) {
+        rect(width/2 - blockW/2, RANGE + gap*2 - i*unitH, blockW, blockH);
+    }    
+    
     
     // bell
     
     fill(BELL_FILL);
     
     // time control by changing the number
-    if (h >= TOP_POINT) {
+    if (currentLevel >= nob) {
         BELL_FILL += 30;
-    } else if (BELL_FILL > 0) {
-        BELL_FILL -= 1;
+    } else if (currentLevel > 0) {
+        BELL_FILL -= 5;
     }
+    rect(width/2 - 50, unitH, 100, 3);
     
-    rect(width/2 - 50, height - 100 - TOP_POINT, 100, 3);
+    // end of the visualizer
+    pop();
+    
+    drawGround();
 }
 
+function drawGround(){
+    fill(0, 170, 255);
+    rect(0, height - 100, width, 100);
+}
 
-
-// function windowResized() {
-//     resizeCanvas(windowWidth, windowHeight);
-// }
+function windowResized() {
+     resizeCanvas(windowWidth, windowHeight);
+ }
