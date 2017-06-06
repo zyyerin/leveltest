a521adac8d21cdf848e7efc9bbc6a41074e5444f
Erin Zhang <yzhang3@cca.edu>
2017-06-06 08:52:34 -0700

    0.1.2
    sensitivity change

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var TRIGGER;
 var RECORDS = [0, 0, 0, 0];
 var RANGE;
 
+// var SLIDER;
 
 function preload() {
     BGIMG = loadImage("assets/bg.png");
@@ -28,20 +29,23 @@ function setup() {
     RANGE = 325;
     BELL_FILL = 0;
     noStroke();
-    
+
     imageMode(CENTER);
+
+    // SLIDER = createSlider(0, 100, 50);
+    // SLIDER.position(50, 50);
 }
 
 function normalTest() {
     background(255);
     let blockW = 25;
-    
+
     // sensitivity adjustment
     let vol = MIC.getLevel();
-    let volScaler = map(vol, 0, 0.2, 1, 2);
+    let volScaler = map(vol, 0, 0.8, 1, 2);
     let spectrum = ARNOLD_FFT.analyze();
-    
-    
+
+
     // records
     let currentRecords = [];
     for (let i = 0; i < spectrum.length; i += 4) {
@@ -52,13 +56,13 @@ function normalTest() {
     // background setting
     push();
     translate(width/2 - 61, height - 90);
-    
+
 //    //range tester
 //    fill(255,0,255);
 //    rect(0,0,10,-RANGE);
-    
+
     // VISUALIZOR starts here
-    
+
     // record session
     fill(255, 200, 0);
     for(let i = 0; i < currentRecords.length; i += 1) {
@@ -66,13 +70,13 @@ function normalTest() {
             RECORDS[i] = currentRecords[i];
         }
     }
-    
+
     for (let i = 0; i < RECORDS.length; i += 1) {
         rect(blockW * i, 0, blockW, -RECORDS[i]);
     }
-    
+
     // spectrum
-    
+
     fill(255, 100, 0);
     for (let i = 0; i < spectrum.length; i += 4) {
         let x = map(i, 0, spectrum.length, 0, blockW * 4);
@@ -80,9 +84,9 @@ function normalTest() {
         rect(x, 0, blockW, -h * volScaler);
     }
 
-    
+
     // end trigger
-    
+
     TRIGGER = (function(){
         for(let i = 0; i < currentRecords.length; i += 1) {
         if (currentRecords[i] < RANGE) {
@@ -91,12 +95,12 @@ function normalTest() {
        return true;
     }
     }());
-    
+
     // time control by changing the number
-    
+
     pop();
     // end of the visualizer
-    
+
     //    tint(255,100);
     image(BGIMG, width/2, height/2);
     image(ARNOLD_HEAD, width/2, 160);
@@ -110,17 +114,18 @@ function loud() {
     translate(width/2, 160);
     rotate(radians(random(-10, 10)));
     image(ARNOLD_HEAD, 0, 0);
-    
+
     translate(0, -50);
     rotate(radians(random(-10, 10)));
     image(ARNOLD_TYPE, 0, 0, random(200, 450), random(45, 150));
-    
+
     pop();
 }
 
 function draw() {
     if (TRIGGER) {
         loud();
+
         if (mouseIsPressed) {
             TRIGGER = false;
             RECORDS = [0, 0, 0, 0];
